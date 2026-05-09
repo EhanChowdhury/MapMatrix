7c6ab514d6a7e88d2e03b6dff185734528a2019c
Ehan Chowdhury <nibizsoft@gmail.com>
2026-05-10 04:01:16 +0600

    update localhost

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 
-const BACKEND = process.env.NEXT_PUBLIC_API_BASE ?? "http://localhost:3000";
+const BACKEND = process.env.NEXT_PUBLIC_API_BASE ?? "http://localhost:3001";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["10.10.10.101", "localhost:3001"],
